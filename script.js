a25cdf531d9128aafbd36f59ec1853bcc549c6e3
yungshun317 <seraph317@gmail.com>
2023-09-29 00:05:00 +0800

    [FEAT] Create the recursive animation

--- script.js
+++ script.js
@@ -11,36 +11,43 @@ let portalType = 0;
 
 document.addEventListener("click", function(e) {
     if (portalType === 0) {
+        // Create red portals
         startX = e.pageX;
         startY = e.pageY;
         svgContainer.innerHTML += `<image id="r1" x="${startX}" y="${startY}" xlink:href="static/Port2.svg"/>`;
         portalType = 1
-    }
-    else {
+    } else {
         timeline.set("#runner", {
             x: startX + 10,
-            y:startY + 40
+            y: startY + 40
         });
 
-        if (portalType === 2) { r.remove(); }
+        if (portalType === 2) {
+            r.remove();
+        }
 
+        // Create blue portals
         endX = e.pageX;
         endY = e.pageY;
         svgContainer.innerHTML += `<image id="r" x="${endX}" y="${endY}" xlink:href="static/Port1.svg"/>`;
         portalType = 2;
 
-        move(0)
+        move(0);
     }
 }, false);
 
-function move() {
-    timeline.to("#runner", {
-        x: endX,
-        y: endY + 40
-    });
+function move(count) {
+    if (count < 10) {
+        timeline.to("#runner", {
+            x: endX,
+            y: endY + 40
+        });
 
-    timeline.set("#runner", {
-        x: startX + 10,
-        y: startY + 40
-    });
+        timeline.set("#runner", {
+            x: startX + 10,
+            y: startY + 40
+        });
+        count += 1;
+        move(count);
+    }
 }
